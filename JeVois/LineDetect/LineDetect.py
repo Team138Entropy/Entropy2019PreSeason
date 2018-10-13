de2867d881c436f986f428ebad7b3325722bf107
maxh65000 <booah8@gmail.com>
2018-10-12 20:31:47 -0400

    Move everything to a single file

--- JeVois/LineDetect/LineDetect.py
+++ JeVois/LineDetect/LineDetect.py
@@ -1,7 +1,24 @@
-import libjevois as jevois
-import cv2
-# import numpy as np
-from maincode import process
+import numpy as np
+import cv2 as cv
+import sys
+try:
+    import libjevois as jevois
+except Exception as e:
+    print("Could not find JeVois!")
+
+PY3 = sys.version_info[0] == 3
+
+# params
+doOtsu = False
+doGuassianBeforeThreshold = True
+doGuassianAfterThreshold = True
+guassianAmount = 21
+# see https://docs.opencv.org/2.4/modules/imgproc/doc/structural_analysis_and_shape_descriptors.html?highlight=findcontours#findcontours
+mode = cv.RETR_LIST
+method = cv.CHAIN_APPROX_SIMPLE
+
+if PY3:
+    xrange = range
 
 # Line detection
 #
@@ -75,7 +92,7 @@ class LineDetect:
         # - http://docs.opencv.org/trunk/d5/d0f/tutorial_py_gradients.html
         # - http://docs.opencv.org/trunk/d7/d4d/tutorial_py_thresholding.html
         #
-        # and so on. When they do "img = cv2.imread('name.jpg', 0)"
+        # and so on. When they do "img = cv.imread('name.jpg', 0)"
         # in these tutorials, the last 0 means they want a
         # gray image, so you should use getCvGRAY() above in these cases.
         # When they do not specify a final 0 in imread()
@@ -84,16 +101,16 @@ class LineDetect:
         # The simplest you could try is:
         #    outimg = inimg
         # which will make a simple copy of the input image to output.
-        outimg = process(inimg)
+        outimg = processImg(inimg)
 
         # Write a title:
-        cv2.putText(outimg, "JeVois LineDetect", (3, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
+        cv.putText(outimg, "JeVois LineDetect", (3, 20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
 
         # Write frames/s info from our timer into the edge map (NOTE: does not account for output conversion time):
         fps = self.timer.stop()
         height = outimg.shape[0]
         # width = outimg.shape[1]
-        cv2.putText(outimg, fps, (3, height - 6), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
+        cv.putText(outimg, fps, (3, height - 6), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
 
         # Convert our output image to video output format and send to host over USB:
         outframe.sendCv(outimg)
@@ -121,3 +138,71 @@ class LineDetect:
     # Internal method that gets invoked as a custom command
     def hello(self):
         return "Hello from python!"
+
+
+def angle_cos(p0, p1, p2):
+    d1, d2 = (p0-p1).astype('float'), (p2-p1).astype('float')
+    return abs(np.dot(d1, d2) / np.sqrt(np.dot(d1, d1)*np.dot(d2, d2)))
+
+
+def find_squares(img):
+    squares = []
+    for gray in cv.split(img):
+        for thrs in xrange(0, 255, 26):
+            if thrs == 0:
+                bin = cv.Canny(gray, 0, 50, apertureSize=5)
+                bin = cv.dilate(bin, None)
+            else:
+                _retval, bin = cv.threshold(gray, thrs, 255, cv.THRESH_BINARY)
+            bin, contours, _hierarchy = cv.findContours(bin, mode, method)
+            for cnt in contours:
+                cnt_len = cv.arcLength(cnt, True)
+                cnt = cv.approxPolyDP(cnt, 0.02*cnt_len, True)
+                if len(cnt) == 4 and cv.contourArea(cnt) > 1000 and cv.isContourConvex(cnt):
+                    cnt = cnt.reshape(-1, 2)
+                    max_cos = np.max([angle_cos(cnt[i], cnt[(i+1) % 4], cnt[(i+2) % 4]) for i in xrange(4)])
+                    if max_cos < 0.1 or True:
+                        squares.append(cnt)
+    return squares
+
+
+def processImg(img):
+    # convert to B&W
+    img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    img = cv.cvtColor(img, cv.COLOR_GRAY2RGB)
+    if doGuassianBeforeThreshold:
+        # Guassian blur
+        img = cv.GaussianBlur(img, (guassianAmount, guassianAmount), 0)
+
+    # threshold color
+    # strict threshold
+    img = cv.threshold(img, 175, 255, cv.THRESH_TOZERO)[1]
+    img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    add = 0
+    if doOtsu:
+        add = cv.THRESH_OTSU
+    # Otsu's thresholding
+    # see https://docs.opencv.org/3.4/d7/d4d/tutorial_py_thresholding.html
+    img = cv.threshold(img, 127, 255, cv.THRESH_BINARY + add)[1]
+
+    if doGuassianAfterThreshold:
+        # Guassian blur
+        img = cv.GaussianBlur(img, (guassianAmount, guassianAmount), 0)
+
+    img = cv.cvtColor(img, cv.COLOR_GRAY2RGB)
+
+    squares = find_squares(img)
+    cv.drawContours(img, squares, -1, (0, 255, 0), 3)
+    return img
+
+
+if __name__ == '__main__':
+    img = cv.imread("test.png")
+    afterImg = processImg(img)
+    cv.imwrite("out.png", afterImg)
+    try:
+        cv.imshow('squares', afterImg)
+        cv.waitKey()
+    except Exception as e:
+        pass
